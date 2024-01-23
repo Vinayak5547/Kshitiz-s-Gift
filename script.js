98eaea0527a918b2adadfbae76961d233850dcb6
Vinayak Sharma <122269287+Vinayak5547@users.noreply.github.com>
2024-01-24 00:04:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function playBirthdayMessage() {
     const birthdayMessage = 'HAPPY BIRTHDAY!';
-    const voices = window.speechSynthesis.getVoices();
-    
-    if (voices.length > 0) {
-        const randomVoice = voices[Math.floor(Math.random() * voices.length)];
-        const msg = new SpeechSynthesisUtterance(birthdayMessage);
-        msg.voice = randomVoice;
-        window.speechSynthesis.speak(msg);
-    } else {
-        alert('Text-to-speech voices not available.');
-    }
+
+    // Wait for the 'voiceschanged' event before getting voices
+    window.speechSynthesis.onvoiceschanged = function () {
+        const voices = window.speechSynthesis.getVoices();
+        
+        if (voices.length > 0) {
+            const randomVoice = voices[Math.floor(Math.random() * voices.length)];
+            const msg = new SpeechSynthesisUtterance(birthdayMessage);
+            msg.voice = randomVoice;
+            window.speechSynthesis.speak(msg);
+        } else {
+            alert('Text-to-speech voices not available.');
+        }
+    };
 }
