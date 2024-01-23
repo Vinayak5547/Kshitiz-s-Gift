be7fa683d0dac7bf578710945ee4b5095f8ff5e4
Vinayak Sharma <122269287+Vinayak5547@users.noreply.github.com>
2024-01-24 00:09:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function playBirthdayMessage() {
     const birthdayMessage = 'HAPPY BIRTHDAY!';
 
-    // Wait for the 'voiceschanged' event before getting voices
-    window.speechSynthesis.onvoiceschanged = function () {
+    setTimeout(() => {
         const voices = window.speechSynthesis.getVoices();
-        
+
         if (voices.length > 0) {
             const randomVoice = voices[Math.floor(Math.random() * voices.length)];
             const msg = new SpeechSynthesisUtterance(birthdayMessage);
@@ -13,5 +12,5 @@ function playBirthdayMessage() {
         } else {
             alert('Text-to-speech voices not available.');
         }
-    };
+    }, 1000); // Adjust the timeout duration as needed
 }
